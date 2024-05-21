b01386d9a97151c9418dd363404abea4a5f10761
Jhon Zambrano Macias <jhonmacias1999@gmail.com>
2024-05-21 10:54:26 -0500

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from config import config
 from flask_mysqldb import MySQL
 
@@ -27,8 +27,41 @@ def listar_productos():
             productos.append(producto)
         return jsonify({'productos': productos, 'mensaje': "Productos Listados"})
     except Exception as es:
+        return jsonify({'mensaje': "Error"})
+
+#producto por id
+@app.route('/product/<id_producto>', methods=['GET'])
+def product_by_id(id_producto):
+    try:
+        cursor = conexion.connection.cursor()
+        sql = "SELECT * FROM product WHERE id_producto = '{0}' ".format(id_producto)
+        cursor.execute(sql)
+        datos = cursor.fetchone()
+        if datos != None:
+            producto={'id_producto':datos[0],'nombreproducto':datos[1], 'precioproducto':datos[2], 'detalleproducto':datos[3],
+                      'ivaproducto':datos[4], 'preciototal':datos[5], 'fechaactualizacion':datos[6], 'fechacreacion':datos[7],
+                      'urlImagen':datos[8], 'sistema':datos[9]}
+            return jsonify({'productos': producto, 'mensaje': "Producto Encontrado"})
+        else:
+            return jsonify({'mensaje': "Producto no encontrado - verifique el id"})
+
+    except Exception as es:
         return jsonify({'mensaje': "Error"})    
 
+#registrar product
+@app.route('/api/register', methods=['POST'])
+def registrar_producto():
+    try:
+        cursor = conexion.connection.cursor()
+        sql = """INSERT INTO product (id_producto,nombreproducto,precioproducto, detalleproducto,ivaproducto, preciototal,fechaactualizacion,fechacreacion,urlImagen,sistema)
+                 VALUES({0},'{1}',{2},'{3}',{4},{5},'{6}','{7}','{8}','{9}')""".format(request.json['id_producto'],request.json['nombreproducto'])
+        cursor.execute(sql)
+        conexion.connection.commit()
+        return jsonify({"mensaje":"Producto Registrador"})
+
+    except Exception as es:
+        print(es)
+        return jsonify({'mensaje': "Error"}) 
 
 
 #mensaje para cuando se accede a la página correcta
