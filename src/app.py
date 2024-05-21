4d52af80a310d6a1ce9f5d43321d5307f47df6ce
Jhon Zambrano Macias <jhonmacias1999@gmail.com>
2024-05-21 09:26:00 -0500

    Create app.py

---
+++ src/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from config import config
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Bienvenido"
+
+
+if __name__ == '__main__':
+    app.config.from_object(config['development'])
+    app.run()
